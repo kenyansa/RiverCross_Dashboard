94e87da7c3617ba85e9def55c23f282a08b90f0c
kenyansa <kenyansajustine@gmail.com>
2024-08-13 18:16:51 +0300

    Add navbar and parent layout to the root

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar";
 
 
 
@@ -5,7 +6,18 @@ export default function App() {
 
   return (
     <>
-      <h1>Hello there</h1>
+      <Navbar />
+      <div className="pt-16 flex h-screen">
+        <div className="flex-1 p-4">
+          <h1 className='text-3xl font-bold mb-2'>DashBoard</h1>
+          <hr className='w-full border-gray-300 border-t-2 mb-4 mx-2'/>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {/* Charts here */}
+          </div>
+
+        </div>
+
+      </div>
         
     </>
   )
