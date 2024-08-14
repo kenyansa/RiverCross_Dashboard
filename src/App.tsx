8b4a2253a8af9617b4d54b691f088a071f058a5d
kenyansa <kenyansajustine@gmail.com>
2024-08-14 07:47:26 +0300

    align vertical line, and date and search component well

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,34 @@ import DoughnutChart from "./components/DoughNut";
 import LyneChart from "./components/LyneChart";
 import Navbar from "./components/Navbar";
 import Notifications from "./components/Notifications";
+import SearchAndDatePicker from "./components/SearchAndDatePicker";
 
 export default function App() {
   return (
     <>
       <Navbar />
-      <div className="pt-16 flex flex-col md:flex-row h-screen">
-        {/* Right Section: Notifications */}
-        <div className='w-full md:w-1/4 p-4 md:order-1'>
-          <Notifications />
-        </div>
-
-        {/* Vertical Line Separator */}
-        <div className="w-1 bg-gray-300 border-t-1 h-full"></div>
-
+      <div className="pt-16 flex flex-col-reverse md:flex-row min-h-screen">
         {/* Left Section: Dashboard Header and Charts */}
         <div className="flex-1 p-4 md:w-3/4">
-          <h1 className='text-3xl font-bold mb-2'>DashBoard</h1>
-          <hr className='w-full border-gray-300 border-t-1 mb-4 mx-2'/>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {/* Charts here */}
+          <h1 className='text-3xl font-bold mb-2 mx-2'>DashBoard</h1>
+          <hr className='w-full border-gray-300 border-t-1 mb-4' />
+          <div className='mx-2'>
+            <SearchAndDatePicker />
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-1">
             <BaChart />
             <AriaChart />
             <LyneChart />
             <DoughnutChart />
           </div>
+        </div>
+
+        {/* Vertical Line Separator */}
+        <div className="hidden md:block w-px bg-gray-300"></div>
+
+        {/* Right Section: Notifications */}
+        <div className='w-full md:w-1/4 p-4'>
+          <Notifications />
         </div>
       </div>
     </>
