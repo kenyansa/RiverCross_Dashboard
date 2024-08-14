f78b84ba6e9ac5556901bde3b164f0974f4b07b7
kenyansa <kenyansajustine@gmail.com>
2024-08-14 14:31:44 +0300

    add dropdown menu to the doughnut

--- src/components/DoughNut.tsx
+++ src/components/DoughNut.tsx
@@ -1,5 +1,5 @@
 import { Doughnut } from 'react-chartjs-2';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend} from 'chart.js';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
@@ -25,9 +25,24 @@ export default function DoughnutChart() {
   return (
     <div className='bg-blue-50 rounded-lg p-4 m-2 relative'>
       <h2 className="text-2xl font-semibold text-gray-800 mb-6">Assets Service Summary</h2>
+      <div className="flex justify-between items-center mb-4">
+        <div className="w-full flex justify-end">
+          <select
+            id="service"
+            name="service"
+            defaultValue="Service"
+            className="px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          >
+            <option value="Service">Service</option>
+            <option value="Maintenance">Maintenance</option>
+            <option value="Repair">Repair</option>
+            <option value="Replacement">Replacement</option>
+          </select>
+        </div>
+      </div>
       <div className="flex justify-center items-center">
         <div className="w-auto h-200">
-             <Doughnut data={data} />
+          <Doughnut data={data} />
         </div>
       </div>
     </div>
